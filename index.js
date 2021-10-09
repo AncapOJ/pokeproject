dc0141f5327ea717e6b6e127106be7b7dc4d89cb
EstherPE <esther.herraiz@bootcamp-upgrade.com>
2021-10-09 19:40:30 +0200

    basic

--- index.js
+++ index.js
@@ -6,14 +6,35 @@ fetch('https://pokeapi.co/api/v2/pokemon/?limit=150')
     console.log(myJson);
     const list = myJson.results;
     console.log(list);
-    let counter=0;
+    let counter=1;
     for (item of list){
         console.log(item);
-        let newp = document.createElement('p');
-        newp.textContent=counter+': '+item.name;
-        document.body.appendChild(newp);
+        let newpoke = document.createElement('div');
+        newpoke.textContent=counter+': '+item.name.toUpperCase();
+        newpoke.className="poke__item";
+        let container = document.querySelector('.poke');
+        container.appendChild(newpoke);
+        fetch(item.url)
+          .then((response) => {
+            return response.json();
+          })
+          .then((myJson) => {
+            for (i of myJson.types){
+                let type = document.createElement('span');
+                type.textContent=i.type.name.toUpperCase();
+                type.className=i.type.name;
+                newpoke.appendChild(type);
+
+
+            }
+            
+            
+          });
         counter++;
     }
+    
+    
+
   });
 
 
